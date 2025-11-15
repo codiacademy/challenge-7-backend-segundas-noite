5cb224bb122fa2fd327893c373bac405f015289f
Janderff <janderffigueiredo@gmail.com>
2025-11-15 10:10:24 -0300

    Feat:Filtro por dia criado

---
+++ api/apps/src/functions/metrics/metrics-expenses/filter-expenses-day.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import { prisma } from '../../../lib/prisma.js'
+import { get } from 'http'
+export const router = express.Router()
+
+router.get('/expenses-day/:date', async (req, res) => {
+  const getDay = new Date(req.params.date)
+  const expense = await prisma.expenses.findMany({
+    where: {
+      date: {
+        gte: getDay ? new Date(getDay.setHours(0, 0, 0, 0)) : undefined,
+        lte: getDay ? new Date(getDay.setHours(23, 59, 59, 59)) : undefined,
+      },
+    },
+  })
+  return res.status(200).json(expense)
+})
