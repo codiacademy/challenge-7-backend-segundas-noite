8767345e4caec15c688fbf3fac366ed083a1af41
vanessamuels <vansamuel7@gmail.com>
2025-11-18 19:29:01 -0300

    Criação de rota para deletar franquia

--- api/apps/src/functions/franchises/delete-franchise.ts
+++ api/apps/src/functions/franchises/delete-franchise.ts
@@ -7,20 +7,20 @@ router.delete('/users/:id', async (req, res) => {
   try {
     const { id } = req.params
 
-    const user = await prisma.collaborator.delete({
+    const franchise = await prisma.franchises.delete({
       where: { id },
     })
 
     return res.status(200).json({
-      message: 'Usuário deletado com sucesso',
-      user,
+      message: 'Franquia deletada com sucesso',
+      franchise,
     })
   } catch (error: any) {
     if (error.code === 'P2025') {
-      return res.status(404).json({ message: 'Usuário nao encontrado' })
+      return res.status(404).json({ message: 'Franquia nao encontrado' })
     }
 
     console.error(error)
-    return res.status(500).json({ message: 'Erro ao deletar usuário' })
+    return res.status(500).json({ message: 'Erro ao deletar franquia' })
   }
 })
