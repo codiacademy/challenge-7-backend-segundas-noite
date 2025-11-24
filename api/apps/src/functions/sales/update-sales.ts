e5b0628503e42ed71ec25123f9a8b737595f3b9b
HiiroHK <gabrielsresende18@gmail.com>
2025-11-24 20:51:46 -0300

    adição da função update-Sales e do client.http

---
+++ api/apps/src/functions/sales/update-sales.ts
@@ -0,0 +1,46 @@
+import express from 'express'
+export const router = express.Router()
+import { prisma } from '../../lib/prisma.js'
+
+router.put('/sales/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const {
+      modalidade,
+      courseId,
+      nomeAluno,
+      email,
+      telefone,
+      valorBruto,
+      valorLiquido,
+    } = req.body
+
+    const updateSale = await prisma.sale.update({
+      where: { id },
+      data: {
+        modalidade,
+        courseId,
+        nomeAluno,
+        email,
+        telefone,
+        valorBruto,
+        valorLiquido,
+      },
+    })
+
+    return res.status(200).json({
+      message: 'Venda atualizada com sucesso',
+      user: updateSale,
+    })
+  } catch (error) {
+    console.log(error)
+
+    if (error instanceof Error) {
+      return res.status(404).json({ message: 'Venda não encontrada' })
+    }
+
+    return res.status(500).json({ message: 'Erro interno' })
+  }
+})
+
+export default router
