c1d0b1a1ecb8632f45d94330f697aa00f2d820a7
Jander Figueiredo <31749575+Janderff@users.noreply.github.com>
2025-11-25 10:41:53 -0300

    Update delete-franchise.ts

--- api/apps/src/functions/franchises/delete-franchise.ts
+++ api/apps/src/functions/franchises/delete-franchise.ts
@@ -3,24 +3,24 @@ export const router = express.Router()
 
 import { prisma } from '../../lib/prisma.js'
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/courses/:id', async (req, res) => {
   try {
     const { id } = req.params
 
-    const franchise = await prisma.franchises.delete({
+    const course = await prisma.courses.delete({
       where: { id },
     })
 
     return res.status(200).json({
-      message: 'Franquia deletada com sucesso',
-      franchise,
+      message: 'Curso deletado com sucesso',
+      course,
     })
   } catch (error: any) {
     if (error.code === 'P2025') {
-      return res.status(404).json({ message: 'Franquia nao encontrado' })
+      return res.status(404).json({ message: 'Curso não encontrado' })
     }
 
     console.error(error)
-    return res.status(500).json({ message: 'Erro ao deletar franquia' })
+    return res.status(500).json({ message: 'Erro ao deletar curso' })
   }
 })
