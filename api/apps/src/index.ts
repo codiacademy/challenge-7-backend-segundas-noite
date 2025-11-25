5aa2489f2ce4e8f44ec230a391da4ca3d1d701d0
Janderff <janderffigueiredo@gmail.com>
2025-11-25 09:01:33 -0300

    Merging

--- api/apps/src/index.ts
+++ api/apps/src/index.ts
@@ -9,7 +9,7 @@ import { router as updateUser } from './functions/users/update-users'
 import { router as createSales } from './functions/sales/create-sales'
 import { router as getAllSales } from './functions/sales/get-all-sales'
 import { router as deleteSales } from './functions/sales/delete-sales'
-import { router as updateSales } from './functions/users/update-sales'
+import { router as updateSales } from './functions/sales/update-sales'
 
 import { router as createExpenses } from './functions/expenses/create-expenses'
 import { router as getAllExpenses } from './functions/expenses/get-all-expenses'
@@ -18,7 +18,7 @@ import { router as deleteExpenses } from './functions/expenses/delete-expenses'
 import { router as createCourses } from './functions/Courses/create-courses'
 import { router as getAllCourses } from './functions/Courses/get-all-courses'
 import { router as deleteCourses } from './functions/Courses/delete-courses'
-import { router as updateCourses } from './functions/users/update-courses'
+import { router as updateCourses } from './functions/Courses/update-courses'
 const app = express()
 
 app.use(express.json())
