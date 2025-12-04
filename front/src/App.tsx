c092186dab4ab67d47cbb20b3f2d1f1c6bf22c87
Janderff <janderffigueiredo@gmail.com>
2025-12-04 19:33:17 -0300

    Startando as conexãoes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,13 @@
-import { Toaster } from 'sonner'
-import { AppRoutes } from './Routes/AppRoutes'
+import { Toaster } from "sonner";
+import { AppRoutes } from "./Routes/AppRoutes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryclient } from "./lib/reactQuery.ts";
 
 export function App() {
   return (
-    <>
-    <AppRoutes />
-    <Toaster 
-    duration={3000}
-    richColors
-    />
-    </>
-  )
+    <QueryClientProvider client={queryclient}>
+      <AppRoutes />
+      <Toaster duration={3000} richColors />
+    </QueryClientProvider>
+  );
 }
-
-
